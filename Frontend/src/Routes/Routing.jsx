4077dc4ba02199b6e05fa6f3770718e8d5a27f3c
yasindupavithra <yasi20000422@gmail.com>
2025-08-22 23:10:40 +0530

    Removed payment and cart related components

--- Frontend/src/Routes/Routing.jsx
+++ Frontend/src/Routes/Routing.jsx
@@ -1,10 +1,8 @@
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../Components/Home/Home';
 import MainProductList from '../Components/ProductList/MainProductList';
 import { MainProductDetail } from '../Components/ProductDetails/MainProductDetail';
-import { MainCheckout } from '../Components/Checkout/MainCheckout';
 import { MainLogin } from '../Components/Login/Mainlogin';
 import { Register } from '../Components/Login/Register';
 import Products from '../Components/Pages/Products';
@@ -13,8 +11,6 @@ import About from '../Components/Pages/About';
 import Contact from '../Components/Pages/Contact';
 import Projects from '../Components/Pages/Projects';
 import Success from '../Components/SucessPage/Success';
-import CardPayment from '../Components/CardPayment/CardPayment';
-import { MainAddress } from '../Components/Address/MainAddress';
 
 const Routing = () => {
   return (
@@ -23,7 +19,6 @@ const Routing = () => {
         <Route path="/" element={<Home />} />
         <Route path="/productlist" element={<MainProductList />} />
         <Route path="/productdetail/:id" element={<MainProductDetail />} />
-        <Route path="/checkout" element={<MainCheckout />} />
         <Route path="/login" element={<MainLogin />} />
         <Route path="/register" element={<Register />} />
         <Route path="/products" element={<Products />} />
@@ -32,8 +27,6 @@ const Routing = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/success" element={<Success />} />
-        <Route path="/cardpayment" element={<CardPayment />} />
-        <Route path="/address" element={<MainAddress />} />
       </Routes>
     </div>
   );
